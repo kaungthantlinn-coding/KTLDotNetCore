8c6e673d94348e55474b49b6bdc5883e3549cb82
kaungthantlinn-coding <kaungthantlinn307@gmail.com>
2024-09-22 12:32:35 +0630

    Add Model

---
+++ KTLDotNetCore.ConsoleApp/Models/BlogDataModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTLDotNetCore.ConsoleApp.Models
+{
+    public class BlogDataModel
+    {
+        
+        public int BlogId { get; set; }
+        
+        public string BlogTitle { get; set; }
+
+        public string BlogAuthor { get; set; }
+
+
+        public string BlogContent { get; set; }
+    }
+}
