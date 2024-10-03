6c33d5992e90f68b8ad29fc2eaae16c01d67e10c
kaungthantlinn-coding <kaungthantlinn307@gmail.com>
2024-10-03 20:17:45 +0630

    feat: fix connect with class library

---
+++ KTLDotNetCore.ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using KTLDotNetCore.Database.Models;
+
+namespace KTLDotNetCore.ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDbContext db = new AppDbContext();
+            var lst =db.TblBlogs.ToList();
+        }
+    }
+}
