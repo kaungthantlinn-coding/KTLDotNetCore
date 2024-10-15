804f8127ca4bff5b185058f1764c2babf990606e
kaungthantlinn-coding <kaungthantlinn307@gmail.com>
2024-10-15 21:20:43 +0630

    Asp.Net Core Web Api (AdoDotNet CRUD)

---
+++ KTLDotNetCore.RestApi/DataModels/BlogDataModel.cs
@@ -0,0 +1,16 @@
+﻿namespace KTLDotNetCore.RestApi.DataModels
+{
+    public class BlogDataModel
+    {
+        public int BlogId { get; set; }
+
+        public string BlogTitle { get; set; }
+
+        public string BlogAuthor { get; set; }
+
+
+        public string BlogContent { get; set; }
+
+        public bool DeleteFlag { get; set; }
+    }
+}
---
+++ KTLDotNetCore.RestApi/ViewModels/BlogViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace KTLDotNetCore.RestApi.ViewModels
+{
+    public class BlogViewModel
+    {
+        public int Id { get; set; }
+
+        public string ?Title { get; set; }
+
+        public string ?Author { get; set; }
+
+
+        public string ?Content { get; set; }
+
+        public bool DeleteFlag { get; set; }
+    }
+}
