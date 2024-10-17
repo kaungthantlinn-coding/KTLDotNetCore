6f2fe3aedb9753b5028dd6a12915d091403bc884
kaungthantlinn-coding <kaungthantlinn307@gmail.com>
2024-10-17 10:03:52 +0630

    add: ToDoList Project, DataModels

---
+++ ToDoListApi/DataModels/TaskCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoListApi.DataModels
+{
+    public class TaskCategory
+    {
+        public int CategoryId { get; set; }
+
+        public string ?CategoryName { get; set; }
+    }
+}
---
+++ ToDoListApi/DataModels/ToDoList.cs
@@ -0,0 +1,17 @@
+﻿namespace ToDoListApi.DataModels
+{
+    public class ToDoList
+    {
+        public int TaskID { get; set; }
+        public string TaskTitle { get; set; } = string.Empty;
+        public string? TaskDescription { get; set; }
+        public int? CategoryID { get; set; }
+        public byte PriorityLevel { get; set; }
+        public string Status { get; set; } = "Pending";
+        public DateTime DueDate { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? CompletedDate { get; set; }
+
+        
+    }
+}
